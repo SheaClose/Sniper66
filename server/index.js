4d5446dec8d9d94cf8db71e83d33ba0b565b429e
SheaClose <sheaclose@gmail.com>
2018-10-12 12:16:54 -0500

    send grid attached

--- server/index.js
+++ server/index.js
@@ -3,15 +3,52 @@ const express = require("express"),
   bodyParser = require("body-parser"),
   port = process.env.PORT || 3003,
   app = express(),
-  path = require("path");
+  path = require("path"),
+  sg = require("sendgrid")(process.env.SENDGRID_API_KEY);
 
 app.use(express.static(path.join(__dirname + "/../build")));
 app.use(bodyParser.json());
 
-app.post("/api/contact", (req, res) => {
-  res.status(200).json("great!");
-});
+app.post(
+  "/api/contact",
+  ({ body: { contentBody, contactName, contactEmail } }, res) => {
+    let { NODE_ENV } = process.env;
 
+    const request = sg.emptyRequest({
+      method: "POST",
+      path: "/v3/mail/send",
+      body: {
+        personalizations: [
+          {
+            to: [
+              {
+                email:
+                  NODE_ENV === "production"
+                    ? "sniper66band@gmail"
+                    : "sheaclose@gmail.com"
+              }
+            ],
+            subject: `New Contact message frm Sniper66.com`
+          }
+        ],
+        from: {
+          email: contactEmail
+        },
+        content: [
+          {
+            type: "text/plain",
+            value: `From: ${contactName}.
+            
+            ${contentBody}`
+          }
+        ]
+      }
+    });
+    sg.API(request, function(error, response) {
+      res.status(200).json(response);
+    });
+  }
+);
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname + "/../build"));
 });
